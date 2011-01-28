d964b18a8b93ec7693838eb3146a7d11bc13df90
David Cramer <dcramer@gmail.com>
2011-01-27 16:12:54 -0800

    Version bump to 0.3.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ class mytest(test):
 
 setup(
     name='selenium-saucelabs-python',
-    version='0.3.0',
+    version='0.3.1',
     author='David Cramer',
     author_email='dcramer@gmail.com',
     url='http://github.com/dcramer/selenium-saucelabs-python',
